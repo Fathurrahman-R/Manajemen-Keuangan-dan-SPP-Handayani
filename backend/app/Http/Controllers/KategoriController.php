23d87b179694e5b39911348996f8692a6bac22fa
Fathurrahman-R <mr.darksot123@gmail.com>
2025-11-13 08:46:50 +0700

    feat: adding kategori api

---
+++ backend/app/Http/Controllers/KategoriController.php
@@ -0,0 +1,95 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\KategoriRequest;
+use App\Http\Resources\KategoriResource;
+use App\Models\Kategori;
+use Illuminate\Http\Exceptions\HttpResponseException;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class KategoriController extends Controller
+{
+    public function index()
+    {
+        $auth = Auth::user();
+        $kategori = Kategori::all();
+        if(!$kategori)
+        {
+            throw new HttpResponseException(response([
+                'errors'=>[
+                    'message'=>[
+                        'belum ada kategori.'
+                    ]
+                ]
+            ],404));
+        }
+        return KategoriResource::collection($kategori);
+    }
+
+    public function create(KategoriRequest $request)
+    {
+        $auth = Auth::user();
+        $data = $request->validated();
+        $kategori = new Kategori();
+        $kategori->save($data);
+        return (new KategoriResource($kategori))->response()->setStatusCode(201);
+    }
+
+    public function get(string $id)
+    {
+        $auth = Auth::user();
+        $kategori = Kategori::query()->find($id);
+        if(!$kategori)
+        {
+            throw new HttpResponseException(response([
+                'errors'=>[
+                    'message'=>[
+                        'kategori tidak ditemukan.'
+                    ]
+                ]
+            ],404));
+        }
+        return (new KategoriResource($kategori))->response()->setStatusCode(200);
+    }
+
+    public function update(KategoriRequest $request, string $id)
+    {
+        $auth = Auth::user();
+        $data = $request->validated();
+        $kategori = Kategori::query()->find($id);
+        if(!$kategori)
+        {
+            throw new HttpResponseException(response([
+                'errors'=>[
+                    'message'=>[
+                        'kategori tidak ditemukan.'
+                    ]
+                ]
+            ],404));
+        }
+        $kategori->update($data);
+        return (new KategoriResource($kategori))->response()->setStatusCode(200);
+    }
+
+    public function delete(string $id)
+    {
+        $auth = Auth::user();
+        $kategori = Kategori::query()->find($id);
+        if(!$kategori)
+        {
+            throw new HttpResponseException(response([
+                'errors'=>[
+                    'message'=>[
+                        'kategori tidak ditemukan.'
+                    ]
+                ]
+            ],404));
+        }
+        $kategori->delete();
+        return response([
+            'data'=>true
+        ], 200);
+    }
+}
