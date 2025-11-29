3a2e37e3d01acee30cc70b7604023400fbc1b67f
Fathurrahman-R <mr.darksot123@gmail.com>
2025-11-29 23:53:26 +0700

    refactor: add parameters & throw Http Response Exception if the app setting is empty

--- backend/app/Http/Controllers/PdfGeneratorController.php
+++ backend/app/Http/Controllers/PdfGeneratorController.php
@@ -2,14 +2,27 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\AppSetting;
+use Dedoc\Scramble\Attributes\HeaderParameter;
 use Illuminate\Http\Exceptions\HttpResponseException;
 use Illuminate\Http\Request;
 use Barryvdh\DomPDF\Facade\Pdf;
 
 class PdfGeneratorController extends Controller
 {
-    public function index(string $kode_pembayaran)
+    #[HeaderParameter('Authorization')]
+    public function get(string $kode_pembayaran)
     {
+        $appSetting = AppSetting::query()->first();
+        if (!$appSetting) {
+            throw new HttpResponseException(response()->json([
+                'errors' => [
+                    'message' => [
+                        'informasi sekolah tidak ditemukan! mohon untuk mengisi informasi sekolah terlebih dahulu.'
+                    ]
+                ]
+            ],404));
+        }
         // Ambil resource kwitansi sebagai array data sederhana
         $resource = PembayaranController::kwitansi($kode_pembayaran); // KwitansiResource
         $data = $resource->toArray(request());
