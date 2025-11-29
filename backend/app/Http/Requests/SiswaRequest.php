b08f0f94e09687190b5a623b8516f3862deb627b
Fathurrahman-R <mr.darksot123@gmail.com>
2025-11-29 23:32:47 +0700

    add: new siswa request & resource

---
+++ backend/app/Http/Requests/SiswaRequest.php
@@ -0,0 +1,148 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\Exceptions\HttpResponseException;
+use Illuminate\Support\Facades\Auth;
+
+class SiswaRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return Auth::user() !== null;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        $requiredMI = request('jenjang') == 'mi' ? 'required' : 'sometimes';
+        $requiredOther = request('jenjang') != 'mi' ? 'required' : 'sometimes';
+        return [
+            'nis' => [
+                'required',
+                'max:20',
+                'regex:/^[0-9]+$/',
+                'min:4'
+            ],
+            'nisn' => [
+                $requiredMI,
+                'max:20',
+                'regex:/^[0-9]+$/',
+                'min:4'
+            ],
+            'nama' => [
+                'required',
+                'max:100',
+                'regex:/^[A-Za-zÀ-ÿ\'\s]+$/u'
+            ],
+            'jenis_kelamin' => [
+                'required',
+                'in:Laki-laki,Perempuan'
+            ],
+            'tempat_lahir' => [
+                'required',
+                'max:100'
+            ],
+            'tanggal_lahir' => [
+                'required',
+                'date',
+                'before:today',
+                'after:1989-12-30'
+            ],
+            'agama' => [
+                'required',
+                'max:50',
+
+            ],
+            'alamat' => [
+                'required'
+            ],
+            // nested ayah
+            'ayah_nama' => [
+                'max:100'
+            ],
+            'ayah_pendidikan' => [
+                'nullable',
+                'max:50'
+            ],
+            'ayah_pekerjaan' => [
+                'nullable',
+                'max:100'
+            ],
+            // nested ibu
+            'ibu_nama' => [
+                'max:100'
+            ],
+            'ibu_pendidikan' => [
+                'nullable',
+                'max:50'
+            ],
+            'ibu_pekerjaan' => [
+                'nullable',
+                'max:100'
+            ],
+            'wali_nama' => [
+                $requiredOther,
+                'max:100'
+            ],
+            'wali_pekerjaan' => [
+                'nullable',
+                'max:100'
+            ],
+            'wali_alamat' => [
+                $requiredOther
+            ],
+            'wali_no_hp' => [
+                $requiredOther,
+                'max:100'
+            ],
+            'wali_keterangan' => [
+                'nullable'
+            ],
+            'kelas_id' => [
+                'required',
+                'exists:kelas,id'
+            ],
+            'kategori_id' => [
+                'required',
+                'exists:kategoris,id'
+            ],
+            'asal_sekolah' => [
+                'nullable',
+                'max:150'
+            ],
+            'kelas_diterima' => [
+                'nullable',
+                'max:10'
+            ],
+            'tahun_diterima' => [
+                'nullable',
+                'date_format:Y',
+                'before_or_equal:' . date('Y')
+            ],
+            'status' => [
+                'nullable',
+                'in:Aktif,Lulus,Pindah,Keluar'
+            ],
+            'keterangan' => [
+                'nullable'
+            ],
+        ];
+    }
+
+    protected function failedValidation(Validator $validator)
+    {
+        throw new HttpResponseException(response([
+            'errors' => $validator->getMessageBag()
+        ], 400));
+    }
+}
---
+++ backend/app/Http/Resources/SiswaResource.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class SiswaResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'id' => $this->id,
+            'nis' => $this->nis,
+            'nisn' => $this->nisn,
+            'nama' => $this->nama,
+            'jenis_kelamin' => $this->jenis_kelamin,
+            'tempat_lahir' => $this->tempat_lahir,
+            'tanggal_lahir' => $this->tanggal_lahir,
+            'agama' => $this->agama,
+            'alamat' => $this->alamat,
+            'ayah' => $this->whenLoaded('ayah', new AyahResource($this->ayah)),
+            'ibu' => $this->whenLoaded('ibu', new IbuResource($this->ibu)),
+            'wali' => $this->whenLoaded('wali', new WaliResource($this->wali)),
+            'jenjang' => $this->jenjang,
+            'kelas' => $this->whenLoaded('kelas', new KelasResource($this->kelas)),
+            'kategori' => $this->whenLoaded('kategori', new KategoriResource($this->kategori)),
+            'asal_sekolah' => $this->asal_sekolah,
+            'kelas_diterima' => $this->kelas_diterima,
+            'tahun_diterima' => $this->tahun_diterima,
+            'status' => $this->status,
+            'keterangan' => $this->keterangan
+        ];
+    }
+}
