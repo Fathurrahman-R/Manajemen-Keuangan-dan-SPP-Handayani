6fae19f558ad57f45cb0c313d87c46d188edf563
Fathurrahman-R <mr.darksot123@gmail.com>
2026-06-22 17:12:06 +0700

    .

---
+++ frontend-v2/resources/views/livewire/partials/disburse-info.blade.php
@@ -0,0 +1,7 @@
+<div class="p-4">
+    <div class="bg-green-50 dark:bg-green-900/20 border border-green-200 dark:border-green-800 rounded-lg p-4 space-y-2">
+        <p class="text-sm text-green-800 dark:text-green-300">Pencairan telah dilakukan.</p>
+        <p class="text-xs text-green-600 dark:text-green-400">Dicairkan oleh: <span class="font-medium">{{ $disbursedBy }}</span></p>
+        <p class="text-xs text-gray-500 dark:text-gray-400">{{ $disbursedAt }}</p>
+    </div>
+</div>
