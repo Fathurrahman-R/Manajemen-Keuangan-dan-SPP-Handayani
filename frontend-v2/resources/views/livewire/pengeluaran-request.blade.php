c61bed17de49a8c9c72276947bc2509fc8bfb9ea
Fathurrahman-R <mr.darksot123@gmail.com>
2026-06-26 15:44:21 +0700

    .

--- frontend-v2/resources/views/livewire/pengeluaran-request.blade.php
+++ frontend-v2/resources/views/livewire/pengeluaran-request.blade.php
@@ -1,3 +1,24 @@
-<div>
-    {{ $this->table }}
+<div class="space-y-4">
+    @if($this->hasTahunAjaranOptions())
+        <x-filament::section>
+            <div class="flex flex-wrap items-center gap-3">
+                <span class="text-sm font-medium text-gray-700 dark:text-gray-300">Periode:</span>
+                <x-filament::input.wrapper>
+                    <x-filament::input.select wire:model.live="selectedTahunAjaranId">
+                        <option value="">Semua Periode</option>
+                        @foreach($tahunAjaranOptions as $option)
+                            <option value="{{ $option['id'] }}">
+                                {{ $option['nama'] }}
+                                {{ $option['status'] === 'Aktif' ? '(Aktif)' : '(Historis)' }}
+                            </option>
+                        @endforeach
+                    </x-filament::input.select>
+                </x-filament::input.wrapper>
+            </div>
+        </x-filament::section>
+    @endif
+
+    <div>
+        {{ $this->table }}
+    </div>
 </div>
