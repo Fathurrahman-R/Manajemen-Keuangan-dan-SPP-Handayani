8834588feb55987e1ba16de6150ba96acd2aa54f
Fathurrahman-R <mr.darksot123@gmail.com>
2026-07-05 23:19:00 +0700

    Menambahkan halaman log notifikasi

---
+++ frontend-v2/app/Filament/Pages/NotificationLogPage.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Filament\Pages;
+
+use App\Helpers\PermissionHelper;
+use Filament\Pages\Page;
+use UnitEnum;
+
+class NotificationLogPage extends Page
+{
+    protected string $view = 'filament.pages.notification-log';
+
+    protected static string | UnitEnum | null $navigationGroup = 'Pengaturan';
+
+    protected static ?string $navigationLabel = 'Log Notifikasi';
+
+    protected static ?string $title = 'Log Notifikasi Email';
+
+    protected static ?string $slug = 'notification-log';
+
+    protected static ?int $navigationSort = 10;
+
+    public static function shouldRegisterNavigation(): bool
+    {
+        return PermissionHelper::has('view-notification-logs');
+    }
+
+    public function mount(): void
+    {
+        abort_if(!PermissionHelper::has('view-notification-logs'), 403);
+    }
+}
---
+++ frontend-v2/resources/views/filament/pages/notification-log.blade.php
@@ -0,0 +1,3 @@
+<x-filament-panels::page>
+    <livewire:notification-log-table />
+</x-filament-panels::page>
